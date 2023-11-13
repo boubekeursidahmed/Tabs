7787acbe4c11ec95e80e4c413d5410393891a95b
sid ahmed <sidou226@gmail.com>
2023-11-13 15:24:08 +0100

    Initial commit

---
+++ app/src/main/java/dz/anpt/tabs/MainActivity.kt
@@ -0,0 +1,27 @@
+package dz.anpt.tabs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        setupWithNavController(bottomNavigationView, navController)
+
+    }
+}
